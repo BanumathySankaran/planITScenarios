96646937728e490fe0884657af36e0cb2f86733d
vikir <vikir@192.168.0.19>
2021-02-07 08:31:42 +1100

    made changes

--- Report/cucumber-reports/report.js
+++ Report/cucumber-reports/report.js
@@ -1,4 +1,218 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("PlanItContactPageScenarios.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("PlanIT_Verifying_Cart.feature");
+formatter.feature({
+  "line": 3,
+  "name": "Plan IT application scenarios",
+  "description": "As an end user I want open the planIT Application and validate the contact and shop pages",
+  "id": "plan-it-application-scenarios",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 2,
+      "name": "@P12324"
+    }
+  ]
+});
+formatter.scenario({
+  "line": 5,
+  "name": "Action 2 - check the shops page and Items added to cart",
+  "description": "",
+  "id": "plan-it-application-scenarios;action-2---check-the-shops-page-and-items-added-to-cart",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.scenarioOutline({
+  "line": 6,
+  "name": "Creation of users",
+  "description": "",
+  "id": "plan-it-application-scenarios;creation-of-users",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 7,
+  "name": "I am on Home Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "I navigate to \"shops\" page",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "click on buy button \u003cnumber of times\u003e on \u003citem\u003e",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "click on the cart menu",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "the items selected are displayed in the cart",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 13,
+  "name": "",
+  "description": "",
+  "id": "plan-it-application-scenarios;creation-of-users;",
+  "rows": [
+    {
+      "cells": [
+        "number of times",
+        "item"
+      ],
+      "line": 14,
+      "id": "plan-it-application-scenarios;creation-of-users;;1"
+    },
+    {
+      "cells": [
+        "2",
+        "\"Funny Cow\""
+      ],
+      "line": 15,
+      "id": "plan-it-application-scenarios;creation-of-users;;2"
+    },
+    {
+      "cells": [
+        "1",
+        "\"Fluffy Bunny\""
+      ],
+      "line": 16,
+      "id": "plan-it-application-scenarios;creation-of-users;;3"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.scenario({
+  "line": 15,
+  "name": "Creation of users",
+  "description": "",
+  "id": "plan-it-application-scenarios;creation-of-users;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 2,
+      "name": "@P12324"
+    }
+  ]
+});
+formatter.step({
+  "line": 7,
+  "name": "I am on Home Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "I navigate to \"shops\" page",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "click on buy button 2 on \"Funny Cow\"",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "click on the cart menu",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "the items selected are displayed in the cart",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "line": 16,
+  "name": "Creation of users",
+  "description": "",
+  "id": "plan-it-application-scenarios;creation-of-users;;3",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 2,
+      "name": "@P12324"
+    }
+  ]
+});
+formatter.step({
+  "line": 7,
+  "name": "I am on Home Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "I navigate to \"shops\" page",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "click on buy button 1 on \"Fluffy Bunny\"",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "click on the cart menu",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "the items selected are displayed in the cart",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.uri("PlanItContactPageScenarios.feature");
 formatter.feature({
   "line": 1,
   "name": "Plan IT application scenarios",
@@ -23,25 +237,13 @@ formatter.step({
   "name": "I navigate to \"contact\" page",
   "keyword": "When "
 });
-formatter.match({
-  "location": "PlanIT_HomePageVerification.i_am_on_Home_Page()"
-});
+formatter.match({});
 formatter.result({
-  "duration": 6869783700,
-  "status": "passed"
+  "status": "undefined"
 });
-formatter.match({
-  "arguments": [
-    {
-      "val": "contact",
-      "offset": 15
-    }
-  ],
-  "location": "PlanIT_HomePageVerification.i_navigate_to_respective_page(String)"
-});
+formatter.match({});
 formatter.result({
-  "duration": 228557300,
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.scenario({
   "line": 9,
@@ -77,32 +279,143 @@ formatter.step({
   "name": "resolve the errors",
   "keyword": "And "
 });
-formatter.match({
-  "location": "PlanITScenarios.click_on_submit_button()"
-});
+formatter.match({});
 formatter.result({
-  "duration": 245563400,
-  "status": "passed"
+  "status": "undefined"
 });
-formatter.match({
-  "location": "PlanITScenarios.validate_the_errors()"
-});
+formatter.match({});
 formatter.result({
-  "duration": 193046700,
-  "status": "passed"
+  "status": "undefined"
 });
-formatter.match({
-  "location": "PlanITScenarios.populate_Mandatory_fields()"
-});
+formatter.match({});
 formatter.result({
-  "duration": 110451600,
-  "status": "passed"
+  "status": "undefined"
 });
-formatter.match({
-  "location": "PlanITScenarios.resolve_the_errors()"
-});
+formatter.match({});
 formatter.result({
-  "duration": 12766662800,
-  "status": "passed"
+  "status": "undefined"
+});
+formatter.background({
+  "line": 4,
+  "name": "User is Logged In",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 5,
+  "name": "I am on Home Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I navigate to \"contact\" page",
+  "keyword": "When "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "line": 17,
+  "name": "Action 2 - Successful Submission",
+  "description": "",
+  "id": "plan-it-application-scenarios;action-2---successful-submission",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 16,
+      "name": "@P12322"
+    }
+  ]
+});
+formatter.step({
+  "line": 19,
+  "name": "Fill up all the mandatory fields with the correct details",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 20,
+  "name": "Submit the details",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 21,
+  "name": "a valid message successful submission message is displayed",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.background({
+  "line": 4,
+  "name": "User is Logged In",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 5,
+  "name": "I am on Home Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I navigate to \"contact\" page",
+  "keyword": "When "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "line": 24,
+  "name": "Action 2 - check the mandatory fields with invalid data",
+  "description": "",
+  "id": "plan-it-application-scenarios;action-2---check-the-mandatory-fields-with-invalid-data",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 23,
+      "name": "@P12323"
+    }
+  ]
+});
+formatter.step({
+  "line": 25,
+  "name": "fill up the contacts page with invalid data",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 26,
+  "name": "error messages should be seen",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
 });
 });
